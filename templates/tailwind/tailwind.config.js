837f3d92ac02f33ed45236c53a9be45026c4b3be
Alex Finnarn <alex.finnarn@gmail.com>
2021-03-31 14:25:22 -0400

    Update tailwind.config.js

--- templates/tailwind/tailwind.config.js
+++ templates/tailwind/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  // Change to where the template files are in your project.
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // For Rails.
+  // purge: ['./app/**/*.html.erb','./app/helpers/**/*.rb','./app/javascript/**/*.js']
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
